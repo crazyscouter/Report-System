53cb105913629ef9794344a12b39bf46ae38efca
crazyscouter <crazyscouter1@gmail.com>
2014-07-11 13:45:41 -0700

    Updated descriptions.

--- report/lua/autorun/report_config.lua
+++ report/lua/autorun/report_config.lua
@@ -2,19 +2,19 @@ report = {};
 
 report.ReportCoolDown = 30 //minutes; how many a player must way before reporting again
 report.SaveInterval = 30 //in seconds
-report.GeneralReasons = {
+report.GeneralReasons = { //reason that are general.
 	"Random Death Match",
 	"Hurting me :(",
 	"Being a general asshat",
 	"Scrub"
 }
-report.AdminGroups = {
+report.AdminGroups = { //put other non-admin groups here that should have access to the admin menu.
 	"supersexyadmin",
 }
-report.SuperAdminGroups = {
+report.SuperAdminGroups = { //put other non-super-admin groups here that should be able to delete reports.
 	"developer",
 }
 
-report.BracketColor = Color(0, 0, 0, 255);
-report.ReportTextColor = Color(255, 25, 25, 255);
-report.MsgColor = Color(255, 255, 255, 255);
+report.BracketColor = Color(0, 0, 0, 255); //color of the brackets in a chat message.
+report.ReportTextColor = Color(255, 25, 25, 255); //color of the "Reports" text in a chat message.
+report.MsgColor = Color(255, 255, 255, 255); //general text color of a message.
